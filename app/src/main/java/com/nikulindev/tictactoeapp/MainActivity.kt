0795ca813c3233771832c67da8f920ee62c9047d
nikulindev <etci3200@gmail.com>
2024-09-21 20:36:51 +0200

    TTT-03 add binding

--- app/src/main/java/com/nikulindev/tictactoeapp/MainActivity.kt
+++ app/src/main/java/com/nikulindev/tictactoeapp/MainActivity.kt
@@ -1,20 +1,27 @@
 package com.nikulindev.tictactoeapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.nikulindev.tictactoeapp.databinding.ActivityMainBinding
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                replace<MainFragment>(R.id.mainContainer)
+            }
         }
+
     }
 }
